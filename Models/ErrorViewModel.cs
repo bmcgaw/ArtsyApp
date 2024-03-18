c3259bf3cc714af09a34a438dc0439a8ccc4f79a
Brian McGaw <bmcgaw91@gmail.com>
2024-03-17 20:36:57 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArtsyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
