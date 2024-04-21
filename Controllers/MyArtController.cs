9f5c9b091b95cbee6bb0d075462aa4b91fae62e9
Brian McGaw <bmcgaw91@gmail.com>
2024-04-21 14:28:11 -0500

    Register page customization

---
+++ Controllers/MyArtController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ArtsyApp.Controllers
+{
+    public class MyArtController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string Jerry, int ID = 1)
+        {
+            ViewData["Jerry"] = "Jerry Seinfeld";
+            ViewData["ID"] = ID + 1;
+            return View();
+        }
+    }
+}
