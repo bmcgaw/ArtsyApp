e4721a7dae8ae49ff9289a1bcd057e6d6bfdbed0
Brian McGaw <bmcgaw@TBB.bank>
2024-05-23 16:25:08 -0500

    Add authorization to edit posts and add main feed page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
+using ArtsyApp.Data;
 using ArtsyApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ArtsyApp.Controllers
@@ -8,15 +10,22 @@ namespace ArtsyApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> MainFeed()
+        {
+            return View(await _context.ArtPost.ToListAsync());
         }
 
         public IActionResult Privacy()
